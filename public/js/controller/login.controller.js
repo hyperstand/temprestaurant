5565d888869e7cb67af3481be22cc118805343f1
Firestanf <goolman1231@gmail.com>
2019-02-12 20:46:23 +0700

    update 11

--- public/js/controller/login.controller.js
+++ public/js/controller/login.controller.js
@@ -6,19 +6,48 @@ function Controller($scope,$timeout,$q,$http,CSRF_TOKEN)
     password:'',
     rememberme:false
     };
+    $scope.input_stat={
+      email:{touched:false,error:0},
+      password:{touched:false,error:0}
+    }
     $scope.Load=false;
     $scope.emailregex=/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-    $scope.Login=function(){
-      $scope.Load=true;
+    $scope.Login=function()
+    {
+    console.log($scope.input_stat);
+    $scope.userForm.email.$validate();
+    $scope.userForm.password.$validate();
+    if($scope.input_stat.email.error > 0  || $scope.input_stat.password.error > 0)
+      {
+        console.log('error');
+      }
+    else
+    {
+            $scope.Load=true;
       $http.post('http://localhost/project/restaurant_level/public/login',{crfs:CSRF_TOKEN,data:$scope.data}).then(function successCallback(response) {
 
-        $scope.Load=true;
+        $scope.Load=false;
       }, function errorCallback(response) {
-        $scope.Load=true;
+        $scope.Load=false;
       });
-
     }
+    }
+
+    $scope.interacted = function(field) {
+      if(field.$name == "email")
+      {
+        $scope.input_stat.email.error=Object.keys(field.$error).length;
+        $scope.input_stat.email.touched=field.$touched;
+      }
+      else if(field.$name == "password")
+      {
+        $scope.input_stat.password.touched=field.$touched;
+        $scope.input_stat.password.error=Object.keys(field.$error).length;
+      }
+      return $scope.submitted || field.$dirty;
+    };
+
     $scope.rememberme=()=>{($scope.data.rememberme)?($scope.data.rememberme=false):($scope.data.rememberme=true);};
   
     // $scope.$watch('error_stat.rememberme', function(newValue, oldValue) {
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -2,10 +2,7 @@
 
 @section('css')
 <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
-<link rel="stylesheet" href="css/linearicons.css">
-<link rel="stylesheet" href="css/owl.carousel.css">
 <link rel="stylesheet" href="css/font-awesome.min.css">
-<link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.52.0/mapbox-gl.css' rel='stylesheet' />
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 <link rel="stylesheet" href="css/nice-select.css">			
 <link rel="stylesheet" href="css/magnific-popup.css">
@@ -18,56 +15,7 @@
 
 
 @section('component') 
-{{-- <div class="auth Loginbody" ng-controller="loginController" >
-        <div class="header">
-                <img src="img/logo.png" alt="" title="" />
-                <h3>Login</h3>
-                <p><% result %></p>
-        </div>
-        <form method="POST" name="userForm" id="Loginbody" ng-submit="Login()">
-                <div class="mt-20">
-                        <input type="email" name="email" 
-                        placeholder="Email address" required
-                         class="single-input" ng-model="email">
-                        <p class="notify"  ng-messages="userForm.email.$error" ng-pattern='/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i'  ng-show="userForm.$submitted ||userForm.email.$touched">
-                           <span class="error" ng-message="pattern" >Email Not Valid</span> 
-                           <span class="error" ng-message="required">Email Field Cannot Be Empty</span> 
-                        </p>
-                </div>
-                <div class="mt-20">
-                                <input type="password" name="password" 
-                                placeholder="Password" 
-                                required class="single-input" name="password" ng-model="password" >
-                                <p class="notify" ng-messages="userForm.password.$error" ng-show="userForm.$submitted ||userForm.password.$touched">
-                                   <span class="error" ng-message="required">Password Field Cannot Be Empty</span> 
-                                </p>
-                </div>
-
-                <div class="mt-20">
-                        <button href=""  class="genric-btn primary">Login</button>
-                </div>
-                <div class="footer">
-                        <div class="remember">
-                                <div class="custom-checkbox ">
-                                        <input type="checkbox" id="confirm-checkbox" ng-model="error_stat.rememberme">
-                                        <label for="confirm-checkbox"></label>
-                                  </div>
-                                <p class="remember-text" ng-click="rememberme()">
-                                    Remember Me
-                                </p> 
-                        </div>
-                        <a href="" class="retrive">Forget Password</a>
-                </div>
-
-                @include('component.spinner');
-        </form>
-
-        <div class="footer-2">
-                <p>Don't Have An Account Sign Up <a href="{{url('register')}}">Here</a></p>
-        </div>
-    </div>  --}}
-
-    <main ng-view></main>
+<main ng-view></main>
 @endsection
 
 
@@ -81,8 +29,7 @@
 <script src="js/easing.min.js"></script>			
 <script src="js/hoverIntent.js"></script>
 <script src="js/superfish.min.js"></script>	
-<script src="js/jquery.magnific-popup.min.js"></script>	
-<script src="js/owl.carousel.min.js"></script>			
+<script src="js/jquery.magnific-popup.min.js"></script>				
 <script src="js/jquery.sticky.js"></script>
 <script src="js/jquery.nice-select.min.js"></script>
 <script>
